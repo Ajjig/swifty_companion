989928cbbacd68b9ce42aa51e9683ad6a02eb6a8
Ajjig <majjig@pm.me>
2025-01-15 18:44:01 +0100

    Integrate app links and set up MultiBlocProvider for user and profile management

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,49 @@
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:swifty_companion/business_logic/cubit/profile_cubit.dart';
+import 'package:swifty_companion/business_logic/cubit/user_cubit.dart';
+import 'package:swifty_companion/data/providers/auth_api_provider.dart';
 import 'package:swifty_companion/presentation/views/login_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  final appLinks = AppLinks();
+  appLinks.uriLinkStream.listen((Uri uri) {
+    AuthApiProvider.callback(uri);
+  });
+
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Swifty companion',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<UserCubit>(
+          create: (context) => UserCubit(),
+        ),
+        BlocProvider<ProfileCubit>(
+          create: (context) => ProfileCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Swifty companion',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => const LoginScreen(),
+        },
       ),
-      home: const LoginScreen(),
     );
   }
 }
