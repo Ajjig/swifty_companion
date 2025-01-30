95918568ff1d622b4caabef65a0e033ad4e2f28e
Ajjig <majjig@pm.me>
2025-01-30 19:38:38 +0100

    Add ProfilePageView widget to display user profile, skills radar chart, projects, and achievements

---
+++ lib/presentation/widgets/profile_page_view.dart
@@ -0,0 +1,126 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:swifty_companion/core/theme.dart';
+import 'package:swifty_companion/data/models/profile_model.dart';
+
+class ProfilePageView extends StatelessWidget {
+  final ProfileModel profile;
+
+  const ProfilePageView({super.key, required this.profile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TabBarView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Center(
+            child: RadarChart(
+              RadarChartData(
+                getTitle: (index, max) {
+                  return RadarChartTitle(
+                    text: profile.cursusUsers[1]['skills'][index]['name']
+                        .toString(),
+                  );
+                },
+                radarBackgroundColor: Colors.white,
+                radarShape: RadarShape.polygon,
+                ticksTextStyle: const TextStyle(color: Colors.transparent),
+                gridBorderData:
+                    const BorderSide(color: Colors.grey, width: 0.5),
+                titleTextStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 10,
+                ),
+                isMinValueAtCenter: true,
+                dataSets: [
+                  RadarDataSet(
+                    entryRadius: 3,
+                    borderWidth: 1,
+                    fillColor: Colors.grey[200],
+                    borderColor: AppTheme.mainColor,
+                    dataEntries: [
+                      for (final skill in profile.cursusUsers[1]['skills'])
+                        RadarEntry(
+                          value: skill['level'],
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListView.builder(
+            itemCount: profile.projectsUsers.length,
+            itemBuilder: (context, index) {
+              final project = profile.projectsUsers[index];
+              return ListTile(
+                title: Text(
+                  project['project']['name'],
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Row(
+                  children: [
+                    Text(
+                      project['status'] ?? 'No status',
+                      style: const TextStyle(
+                        color: AppTheme.mainColor,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      project['validated?'] != null
+                          ? 'Validated with ${project['final_mark'] ?? 0} score'
+                          : 'Not validated',
+                      style: TextStyle(
+                        color: project['validated?'] != null
+                            ? Colors.green
+                            : Colors.orange,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          ListView.builder(
+            itemCount: profile.achievements.length,
+            itemBuilder: (context, index) {
+              final achievement = profile.achievements[index];
+              return ListTile(
+                title: Text(
+                  achievement['name'],
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Text(
+                  achievement['description'],
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                leading: SvgPicture.network(
+                  achievement['image']
+                      .toString()
+                      .replaceFirst('/uploads/', 'https://cdn.intra.42.fr/'),
+                  width: 50,
+                  height: 50,
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(Icons.error);
+                  },
+                  placeholderBuilder: (context) {
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
