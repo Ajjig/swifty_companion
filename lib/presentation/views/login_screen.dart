c929fb3207ff19fef3aa7e18347159a167f9847a
Ajjig <majjig@pm.me>
2025-01-30 23:45:07 +0100

    Refactor LoginScreen: update welcome message, improve button styling, and remove app bar

--- lib/presentation/views/login_screen.dart
+++ lib/presentation/views/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swifty_companion/business_logic/cubit/user_cubit.dart';
+import 'package:swifty_companion/core/theme.dart';
 import 'package:swifty_companion/presentation/widgets/user_loaded_widget.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -19,9 +20,6 @@ class LoginScreen extends StatelessWidget {
         return UserLoadedWidget(profile: state.profile);
       } else {
         return Scaffold(
-          appBar: AppBar(
-            title: const Text('Login'),
-          ),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -29,17 +27,28 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 const Text(
-                  'Welcome to Intra 42',
+                  'Welcome to Swifty Companion',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 24,
+                    color: AppTheme.secondaryColor,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () => context.read<UserCubit>().login(),
-                  child: const Text('Login with your intra'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppTheme.mainColor,
+                    minimumSize: const Size(220, 40),
+                    maximumSize: const Size(220, 40),
+                  ),
+                  child: const Text(
+                    'Login with intra42',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
             ),
