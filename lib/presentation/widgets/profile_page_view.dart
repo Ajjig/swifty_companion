520740ea17893cbd8447f1f28629f6cf2e3c4ae6
Ajjig <majjig@pm.me>
2025-01-31 16:33:56 +0100

    Update ProfilePageView to utilize lastCursusIndex for skill display and enhance radar chart logic

--- lib/presentation/widgets/profile_page_view.dart
+++ lib/presentation/widgets/profile_page_view.dart
@@ -15,62 +15,104 @@ class ProfilePageView extends StatelessWidget {
       child: TabBarView(
         physics: const BouncingScrollPhysics(),
         children: [
-          RadarChart(
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.easeInOut,
-            RadarChartData(
-              // Add this line to set fixed max value
-              tickCount: 3,
-              ticksTextStyle: const TextStyle(
-                color: Colors.black,
-                fontSize: 5,
-              ),
-              titlePositionPercentageOffset: 0.1,
-              radarBorderData:
-                  const BorderSide(color: Colors.black, width: 0.75),
-              tickBorderData:
-                  const BorderSide(color: AppTheme.secondaryColor, width: 0.1),
-              getTitle: (index, max) {
-                return RadarChartTitle(
-                  text: profile.cursusUsers[1]['skills'][index]['name']
-                      .toString(),
-                );
-              },
-              radarBackgroundColor: Colors.white,
-              radarShape: RadarShape.polygon,
-              gridBorderData: const BorderSide(color: Colors.grey, width: 0.5),
-              titleTextStyle: const TextStyle(
-                color: AppTheme.secondaryColor,
-                fontSize: 6.5,
-              ),
-              isMinValueAtCenter: false,
-              dataSets: [
-                RadarDataSet(
-                  entryRadius: 0,
-                  borderWidth: 0,
-                  borderColor: Colors.transparent,
-                  fillColor: Colors.transparent,
-                  dataEntries: [
-                    for (final _ in profile.cursusUsers[1]['skills'])
-                      RadarEntry(
-                        value: 21,
+          (profile.cursusUsers[profile.lastCursusIndex]['skills'] != null &&
+                  profile.cursusUsers[profile.lastCursusIndex]['skills']
+                          .length >
+                      2)
+              ? RadarChart(
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeInOut,
+                  RadarChartData(
+                    // Add this line to set fixed max value
+                    tickCount: 3,
+                    ticksTextStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 5,
+                    ),
+                    titlePositionPercentageOffset: 0.1,
+                    radarBorderData:
+                        const BorderSide(color: Colors.black, width: 0.75),
+                    tickBorderData: const BorderSide(
+                        color: AppTheme.secondaryColor, width: 0.1),
+                    getTitle: (index, max) {
+                      return RadarChartTitle(
+                        text: profile.cursusUsers[profile.lastCursusIndex]
+                                ['skills'][index]['name']
+                            .toString(),
+                      );
+                    },
+                    radarBackgroundColor: Colors.white,
+                    radarShape: RadarShape.polygon,
+                    gridBorderData:
+                        const BorderSide(color: Colors.grey, width: 0.5),
+                    titleTextStyle: const TextStyle(
+                      color: AppTheme.secondaryColor,
+                      fontSize: 6.5,
+                    ),
+                    isMinValueAtCenter: false,
+                    dataSets: [
+                      RadarDataSet(
+                        entryRadius: 0,
+                        borderWidth: 0,
+                        borderColor: Colors.transparent,
+                        fillColor: Colors.transparent,
+                        dataEntries: [
+                          for (final _ in profile
+                              .cursusUsers[profile.lastCursusIndex]['skills'])
+                            RadarEntry(
+                              value: 21,
+                            ),
+                        ],
                       ),
-                  ],
-                ),
-                RadarDataSet(
-                  entryRadius: 2.5,
-                  borderWidth: 1,
-                  borderColor: AppTheme.mainColor,
-                  dataEntries: [
-                    for (final skill in profile.cursusUsers[1]['skills'])
-                      RadarEntry(
-                        value: skill['level'],
+                      RadarDataSet(
+                        entryRadius: 2.5,
+                        borderWidth: 1,
+                        borderColor: AppTheme.mainColor,
+                        dataEntries: [
+                          for (final skill in profile
+                              .cursusUsers[profile.lastCursusIndex]['skills'])
+                            RadarEntry(
+                              value: skill['level'] ?? 0,
+                            ),
+                        ],
                       ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+                    ],
+                  ),
+                )
+              : profile.cursusUsers[profile.lastCursusIndex]['skills'] != null
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (final skill in profile
+                            .cursusUsers[profile.lastCursusIndex]['skills'])
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                skill['name'].toString(),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                skill['level'].toString(),
+                                style: const TextStyle(
+                                  color: AppTheme.mainColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                      ],
+                    )
+                  : const Center(
+                      child: Text(
+                        'No skills available',
+                        style: TextStyle(
+                          color: AppTheme.mainColor,
+                        ),
+                      ),
+                    ),
           ListView.builder(
             itemCount: profile.projectsUsers.length,
             itemBuilder: (context, index) {
