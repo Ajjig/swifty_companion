8b67340b4567592514ff5332df62012dc0e1e771
Ajjig <majjig@pm.me>
2025-01-31 14:25:42 +0100

    Refactor main.dart: move dotenv.load call to the correct position for proper environment variable loading

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   final appLinks = AppLinks();
   appLinks.uriLinkStream.listen((Uri uri) {
     AuthApiProvider.callback(uri);
   });
   await AuthApiProvider.init();
-
-  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
