26a2394703014bb8baed40972f5485132e6d979e
Ajjig <majjig@pm.me>
2025-01-31 01:11:41 +0100

    Refactor HomeScreen: add ProfileDetails widget for enhanced profile display and remove unused LiquidPainter class

--- lib/presentation/views/home_screen.dart
+++ lib/presentation/views/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:swifty_companion/business_logic/cubit/profile_cubit.dart';
 import 'package:swifty_companion/business_logic/cubit/user_cubit.dart';
 import 'package:swifty_companion/core/theme.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:swifty_companion/presentation/widgets/liquid_painer.dart';
+import 'package:swifty_companion/presentation/widgets/profile_details.dart';
 import 'package:swifty_companion/presentation/widgets/profile_page_view.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -187,6 +189,17 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey[500],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 60,
+                  child: ProfileDetails(profile: profile),
+                ),
                 DefaultTabController(
                   length: 3,
                   child: Expanded(
@@ -216,42 +229,4 @@ class HomeScreen extends StatelessWidget {
       },
     );
   }
-}
-
-class LiquidPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..shader = const LinearGradient(
-        colors: [AppTheme.mainColor, Color(0xFF5FC6FF)],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      ).createShader(Rect.fromLTWH(0, 0, size.width, size.height));
-
-    final path = Path()
-      ..lineTo(0, size.height * 0.8)
-      ..cubicTo(
-        size.width * 0.25,
-        size.height * 0.65,
-        size.width * 0.25,
-        size.height * 0.35,
-        size.width * 0.5,
-        size.height * 0.5,
-      )
-      ..cubicTo(
-        size.width * 0.75,
-        size.height * 0.65,
-        size.width * 0.75,
-        size.height * 0.35,
-        size.width,
-        size.height * 0.5,
-      )
-      ..lineTo(size.width, 0)
-      ..close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
